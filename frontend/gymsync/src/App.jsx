f9b16d3da1049408f25886a86a58581b3cc35483
mncrbx <corbellinivinicius4@gmail.com>
2026-06-08 17:41:15 -0300

    feat(frontend): first commit

---
+++ frontend/gymsync/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+	return (
+		<>
+			<div>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum delectus quaerat, sapiente fugiat tempora corporis nobis deserunt quis provident accusamus minima, maxime in, corrupti rerum quasi! Exercitationem ab voluptas quaerat.</p>
+			</div>
+		</>
+	)
+	
+}
+
+export default App
