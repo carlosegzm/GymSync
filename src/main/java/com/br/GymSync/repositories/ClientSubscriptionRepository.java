2338a90d27f7896d486fb09f7f2528002246c8e0
cadu <boli69112@gmail.com>
2026-06-12 08:39:59 -0300

    feat: add new methods to repositories

--- src/main/java/com/br/GymSync/repositories/ClientSubscriptionRepository.java
+++ src/main/java/com/br/GymSync/repositories/ClientSubscriptionRepository.java
@@ -5,10 +5,13 @@ import com.br.GymSync.domain.enums.SubscriptionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ClientSubscriptionRepository extends JpaRepository<ClientSubscription, UUID> {
     Optional<ClientSubscription> findByClientIdAndStatus(UUID clientId, SubscriptionStatus status);
+    long countByStatusAndEndDateBetween(SubscriptionStatus status, LocalDate start, LocalDate end);
+    long countByStatus(SubscriptionStatus status);
 }
