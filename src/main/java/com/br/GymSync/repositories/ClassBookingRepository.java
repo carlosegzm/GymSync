e7ca5f4e27c240c3c3e085ecaf9e1b9eea5a551f
cadu <boli69112@gmail.com>
2026-06-15 19:37:40 -0300

    feat(repository): add findByGroupClassId method

--- src/main/java/com/br/GymSync/repositories/ClassBookingRepository.java
+++ src/main/java/com/br/GymSync/repositories/ClassBookingRepository.java
@@ -4,7 +4,10 @@ import com.br.GymSync.domain.entities.ClassBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClassBookingRepository extends JpaRepository<ClassBooking, Long> {
     long countByGroupClassId(Long groupClassId);
+    List<ClassBooking> findByGroupClassId(Long groupClassId);
 }
